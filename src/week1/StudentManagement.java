a8012759e4fabb1926e4feb86095fd83592c3b88
Nguyễn Đức Hiếu <36853039+hieuss99@users.noreply.github.com>
2018-09-20 09:35:51 +0700

    Update StudentManagement.java

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -152,26 +152,33 @@ public class StudentManage {
     public static void main(String[] args) {
         // TODO:
     	String a;
-//   
-        list[0] = new Student("y","1","y@vnu");
-	    
-        list[1] = new Student("b","2","b@vnu");
-	    
-        list[2] = new Student("c","3","c@vnu");
-	    
-        list[3] = new Student("d","4","d@vnu");
-	    
-        list[4] = new Student("e","5","e@vnu");
-	    
-        list[5] = new Student("f","6","f@vnu");
+//        Student persion1 = new Student();	
+//        Student persion2 = new Student("ND Hieu","17020742","17020742@vnu.edu.vn");
+//        persion1.Name = "Nguyen Duc Hieu	";
+//        persion1.Id = "17020742";
+//        persion1.Email = "17020742@vnu.edu.com";
+//        persion1.Group = "INT22041";
+//        Student persion3 = new Student(persion1);
+//        persion3.Group = "INT22042";
+//        // print name:
+//        System.out.println("Name: "+persion1.Name);
+//        persion1.getInfo();
+//        //check Group:
+//        System.out.println("Same Group: "+sameGroup(persion1,persion2));
+        list[0] = new Student("hieu","1","hieu@vnu");
+        list[1] = new Student("hieu","2","hieu@vnu");
+        list[2] = new Student("hieu","3","hieu@vnu");
+        list[3] = new Student("hieu","4","hieu@vnu");
+        list[4] = new Student("hieu","5","hieu@vnu");
+        list[5] = new Student("hieu","6","hieu@vnu");
         list[5].setGroup("INT22042");
-        list[6] = new Student("g","7","g@vnu");
+        list[6] = new Student("hieu","7","hieu@vnu");
         list[6].setGroup("INT22042");
-        list[7] = new Student("h","8","h@vnu");
+        list[7] = new Student("hieu","8","hieu@vnu");
         list[7].setGroup("INT22042");
-        list[8] = new Student("i","9","i@vnu");
+        list[8] = new Student("hieu","9","hieu@vnu");
         list[8].setGroup("INT22043");
-        list[9] = new Student("k","10","k@vnu");
+        list[9] = new Student("hieu","10","hieu@vnu");
         list[9].setGroup("INT22043");
         Test1.studentsByGroup();
         System.out.print("remove the student: ");
