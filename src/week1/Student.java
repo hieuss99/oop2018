e4a463db376d2b44a32c4ef8a472e68198ccf6d4
Nguyễn Đức Hiếu <36853039+hieuss99@users.noreply.github.com>
2018-09-20 09:00:44 +0700

    Update Student.java

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -1,38 +1,31 @@
-package week1;
-
-public class Student {
-
-    // TODO: khai báo các thuộc tính cho Student
-
-    // TODO: khai báo các phương thức getter, setter cho Student
-
-    /**
-     * Constructor 1
-     */
-    Student() {
+public Student() {
         // TODO:
+        this.Name = "student";
+        this.Id ="000";
+        this.Group = "INT22041";
+        this.Email = "uet@vnu.edu.vn";
     }
-
-    /**
+     /**
      * Constructor 2
      * @param n
      * @param sid
      * @param em
      */
-    Student(String n, String sid, String em) {
+    public Student(String n, String i, String em) {
         // TODO:
+        this.Name = n;
+        this.Id = i;
+        this.Group = "INT22041";
+        this.Email = em;
     }
-
-    /**
+     /**
      * Constructor 3
      * @param s
      */
-    Student(Student s) {
+     public Student(Student s) {
         // TODO:
+        this.Name = s.Name;
+        this.Id = s.Id;
+        this.Email = s.Email;
+        this.Group = s.Group;
     }
-
-    String getInfo() {
-        // TODO:
-        return null; // xóa dòng này sau khi cài đặt
-    }
-}
