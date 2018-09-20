0b60dfeec261df02539864250aada315b824540d
Nguyễn Đức Hiếu <36853039+hieuss99@users.noreply.github.com>
2018-09-20 08:58:14 +0700

    Update StudentManagement.java

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -42,41 +42,6 @@ class Student {
      * Constructor 1
      */
 
-    public Student() {
-        // TODO:
-        this.Name = "student";
-        this.Id ="000";
-        this.Group = "INT22041";
-        this.Email = "uet@vnu.edu.vn";
-    }
-
-    /**
-     * Constructor 2
-     * @param n
-     * @param sid
-     * @param em
-     */
-    public Student(String n, String i, String em) {
-        // TODO:
-        this.Name = n;
-        this.Id = i;
-        this.Group = "INT22041";
-        this.Email = em;
-    }
-
-    /**
-     * Constructor 3
-     * @param s
-     */
-
-    public Student(Student s) {
-        // TODO:
-        this.Name = s.Name;
-        this.Id = s.Id;
-        this.Email = s.Email;
-        this.Group = s.Group;
-    }
-
     public void getInfo()
     {
     	System.out.println("--------------------------------");
@@ -116,6 +81,42 @@ public class StudentManage {
     		list[i].Group = sc.nextLine();
     	}
     }
+	
+    public Student() {
+        // TODO:
+        this.Name = "student";
+        this.Id ="000";
+        this.Group = "INT22041";
+        this.Email = "uet@vnu.edu.vn";
+    }
+
+    /**
+     * Constructor 2
+     * @param n
+     * @param sid
+     * @param em
+     */
+    public Student(String n, String i, String em) {
+        // TODO:
+        this.Name = n;
+        this.Id = i;
+        this.Group = "INT22041";
+        this.Email = em;
+    }
+
+    /**
+     * Constructor 3
+     * @param s
+     */
+
+    public Student(Student s) {
+        // TODO:
+        this.Name = s.Name;
+        this.Id = s.Id;
+        this.Email = s.Email;
+        this.Group = s.Group;
+    }
+
      static void studentsByGroup() {
         // TODO:
     	HashSet<String> hs = new HashSet<String>();
